21878042a5de0e85e4f1419008260f3294353086
Lucas Nascimento <lucas.nascimento@pearson.com>
2014-01-08 08:59:01 -0200

    Complementando com os cenários solicitados pelo Humberto @ Christus

--- src/main/java/br/com/pearson/passaporte/test/api/apps/CriarUsuarioTest.java
+++ src/main/java/br/com/pearson/passaporte/test/api/apps/CriarUsuarioTest.java
@@ -13,30 +13,64 @@ import br.com.pearson.passaporte.test.api.dto.UsuarioDTO;
 
 public class CriarUsuarioTest {
 	
-	public static final String TOKEN = "aa7088cf-f46e-47d2-83e0-f44a8b14436e";
-	public static final String SECRET = "%2Bcp3B%2B18i8WpqqGL%2FCygPk3a5wWhYXDpyjTJsEglzPj%2BINHCAKJalPN45nkWda4tIAGMzpFa60DyFx72X0fmd%2FiQrd5JLKDRyY3LmTqSsKI%3D";
-	public static final String BASE_URL = "http://coc.dclick.com.br/giul/api";
+	public static final String TOKEN = "9c9dd1bb-0aa7-414e-acc1-cfc68cb1388d";
+	public static final String SECRET = "DtCNKPBh4KEwd%2F1ZacrQDNVNxHeU57PMfFG0wEM3eOWcxx204Id4VxWaN7aSYhomgjyd814LGAVBPltlMJUERnVACXwufvcEDpSpPToE1m8%3D";
+	public static final String BASE_URL = "http://homolog.passaporte.coc.com.br/giul/api";
 	
-	public static final Long GRUPO_ECONOMICO_ID = 1L;
-	public static final Long ESCOLA_ID = 1L;
-	public static final Long ESTRUTURA_ID = 17L;
-	public static final Long PERFIL_ESCOLA_ID = 1L;
+	public static final Long GRUPO_ECONOMICO_ID = 2L;
+	public static final Long ESCOLA_ID = 6L;
+	public static final Long ESTRUTURA_ID = 1341L;
+	public static final Long PERFIL_ESCOLA_ID = 28L;
 
 	public static void main(String[] args) throws Exception {
+		
+		//Instanciando os dados necessário para o API Call
 		APICall apiCall = new APICall(BASE_URL);
 		OAuthConsumer consumer = new OAuthConsumer("key", "secret");
 		OAuthToken accessToken = new OAuthToken(TOKEN, SECRET, TokenType.ACCESS);
+		
+		//Classe utilitária que será responsável pela conversão de JSON para objeto e vice-versa - API JACKSON
 		ObjectMapper mapper = new ObjectMapper();
 		
-		UsuarioDTO usuario = new UsuarioDTO("Saulo Brust - Teste - 1", "brust.20", "1234");
-		ClassificacaoDTO classificacao = new ClassificacaoDTO(ESCOLA_ID, ESTRUTURA_ID, PERFIL_ESCOLA_ID);
-		usuario.addClassificacao(classificacao);
-		String request = mapper.writeValueAsString(usuario);
-		
+		//Setup do GRUPO ECONOMICO, necessário passar em todos os HEADER
 		MultiValueMap<String, String> headers = new HttpHeaders();
 		headers.add("X-GRUPO-ECONOMICO", GRUPO_ECONOMICO_ID.toString());		
 		
+		//Instânciando um novo usuário
+		UsuarioDTO usuario = new UsuarioDTO("Lucas Nascimento", "lucas.nascimento123", "1234");
+		ClassificacaoDTO classificacao = new ClassificacaoDTO(ESCOLA_ID, ESTRUTURA_ID, PERFIL_ESCOLA_ID);
+		usuario.addClassificacao(classificacao);
+
+		//Escrevendo o DTO como JSON
+		String request = mapper.writeValueAsString(usuario);
+		
+		//Chamndo a API para inserir e guardando o resultado o JSON
 		String result = apiCall.put("/usuario", request, null, consumer, accessToken, headers);
-		System.out.println(result);
+		
+		//Convertendo o Resultado do JSON para o DTO
+		UsuarioDTO usuarioInserido = mapper.readValue(result, UsuarioDTO.class);
+		
+		//Recuperando esse usuário que foi inserido através da API -ara exemplificar e já transformando no DTO
+		UsuarioDTO usuarioInseridoFromAPI = mapper.readValue( apiCall.get("/usuario/"+ usuarioInserido, null, consumer, accessToken), UsuarioDTO.class);
+		
+		//Criando uma nova Classificiação
+		ClassificacaoDTO classificacaoNova = new ClassificacaoDTO(ESCOLA_ID, ESTRUTURA_ID, PERFIL_ESCOLA_ID);
+		
+		//Adicionando no Usuário para posterior envio do usuário
+		usuarioInseridoFromAPI.addClassificacao(classificacaoNova);
+		
+		String requestToUpdate = mapper.writeValueAsString(usuarioInseridoFromAPI);
+		
+		//Chamando a API para update
+		String resultUpdate = apiCall.put("/usuario",requestToUpdate, null, consumer, accessToken, headers);
+
+		UsuarioDTO usuarioAlterado = mapper.readValue( resultUpdate, UsuarioDTO.class);
+		System.out.println(usuarioAlterado);
+		
+		//Chamando API para excluir o usuário que acabou de ser adicionado e alterado.
+		String resultDelete = apiCall.delete("/usuario", resultUpdate, null, consumer, accessToken, headers);
+		System.out.println(resultDelete);
+		
+
 	}
 }
